192fe30f8b19683f75c7da72377e01d90b5201f5
vicard <vicard@polytech.unice.fr>
2014-11-17 20:39:27 +0100

    Blocage de l'ecran de menu.

--- app/build/generated/source/r/debug/com/bravelittlescientist/android_puzzle_view/R.java
+++ app/build/generated/source/r/debug/com/bravelittlescientist/android_puzzle_view/R.java
@@ -12,14 +12,11 @@ public final class R {
         public static final int push_up_in=0x7f040000;
         public static final int push_up_out=0x7f040001;
     }
-    public static final class array {
-        public static final int integer_array_name=0x7f060000;
-    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f070000;
-        public static final int activity_vertical_margin=0x7f070001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
@@ -34,14 +31,14 @@ public final class R {
         public static final int icon=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0007;
-        public static final int button=0x7f0a0000;
-        public static final int button2=0x7f0a0001;
-        public static final int button3=0x7f0a0002;
-        public static final int button4=0x7f0a0004;
-        public static final int button5=0x7f0a0003;
-        public static final int button6=0x7f0a0005;
-        public static final int puzzle_surface=0x7f0a0006;
+        public static final int action_settings=0x7f090007;
+        public static final int button=0x7f090000;
+        public static final int button2=0x7f090001;
+        public static final int button3=0x7f090002;
+        public static final int button4=0x7f090004;
+        public static final int button5=0x7f090003;
+        public static final int button6=0x7f090005;
+        public static final int puzzle_surface=0x7f090006;
     }
     public static final class layout {
         public static final int activity_game_menu=0x7f030000;
@@ -50,8 +47,8 @@ public final class R {
         public static final int puzzle_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int game_menu=0x7f090000;
-        public static final int launch=0x7f090001;
+        public static final int game_menu=0x7f080000;
+        public static final int launch=0x7f080001;
     }
     public static final class raw {
         public static final int applaudissements=0x7f050000;
@@ -59,13 +56,13 @@ public final class R {
         public static final int cheval=0x7f050002;
     }
     public static final class string {
-        public static final int action_settings=0x7f080000;
-        public static final int app_name=0x7f080001;
-        public static final int choice_img=0x7f080002;
-        public static final int hello=0x7f080003;
-        public static final int hello_world=0x7f080004;
-        public static final int placeholder=0x7f080005;
-        public static final int title_activity_game_menu=0x7f080006;
-        public static final int title_activity_launch=0x7f080007;
+        public static final int action_settings=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int choice_img=0x7f070002;
+        public static final int hello=0x7f070003;
+        public static final int hello_world=0x7f070004;
+        public static final int placeholder=0x7f070005;
+        public static final int title_activity_game_menu=0x7f070006;
+        public static final int title_activity_launch=0x7f070007;
     }
 }
