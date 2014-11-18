3b3de09dc41788350de6d05d83cc24154c223e80
vicard <vicard@polytech.unice.fr>
2014-11-18 23:11:41 +0100

    Ajout animation titre.

--- app/build/generated/source/r/debug/com/bravelittlescientist/android_puzzle_view/R.java
+++ app/build/generated/source/r/debug/com/bravelittlescientist/android_puzzle_view/R.java
@@ -9,8 +9,7 @@ package com.bravelittlescientist.android_puzzle_view;
 
 public final class R {
     public static final class anim {
-        public static final int push_up_in=0x7f040000;
-        public static final int push_up_out=0x7f040001;
+        public static final int fade_in=0x7f040000;
     }
     public static final class attr {
     }
@@ -28,24 +27,26 @@ public final class R {
         public static final int cheval_miniature=0x7f020006;
         public static final int chien=0x7f020007;
         public static final int chien_miniature=0x7f020008;
-        public static final int darkwood=0x7f020009;
-        public static final int directory_icon=0x7f02000a;
-        public static final int directory_up=0x7f02000b;
-        public static final int elephant=0x7f02000c;
-        public static final int elephant_miniature=0x7f02000d;
-        public static final int file_icon=0x7f02000e;
-        public static final int happy_kitten=0x7f02000f;
-        public static final int icon=0x7f020010;
+        public static final int choix_image=0x7f020009;
+        public static final int darkwood=0x7f02000a;
+        public static final int directory_icon=0x7f02000b;
+        public static final int directory_up=0x7f02000c;
+        public static final int elephant=0x7f02000d;
+        public static final int elephant_miniature=0x7f02000e;
+        public static final int file_icon=0x7f02000f;
+        public static final int happy_kitten=0x7f020010;
+        public static final int icon=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
+        public static final int action_settings=0x7f090008;
         public static final int button=0x7f090000;
         public static final int button2=0x7f090001;
         public static final int button3=0x7f090002;
         public static final int button4=0x7f090003;
         public static final int button5=0x7f090004;
         public static final int button6=0x7f090005;
-        public static final int puzzle_surface=0x7f090006;
+        public static final int puzzle_surface=0x7f090007;
+        public static final int title=0x7f090006;
     }
     public static final class layout {
         public static final int activity_game_menu=0x7f030000;
--- app/src/main/java/com/bravelittlescientist/android_puzzle_view/LaunchActivity.java
+++ app/src/main/java/com/bravelittlescientist/android_puzzle_view/LaunchActivity.java
@@ -7,17 +7,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import java.io.IOException;
 
 
 public class LaunchActivity extends Activity {
 
+    // Animation
+    Animation animFadein;
+    private TextView title ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        title = (TextView) findViewById(R.id.title);
+        // load the animation
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+        title.startAnimation(animFadein);
+
+
     }
 
     public void launchPuzzle(View v) {
